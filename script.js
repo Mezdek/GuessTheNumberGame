9d7d74548099a2aff6426474bdbda690a4e3207a
Mez Dek <mezdekosman@hotmail.com>
2021-09-27 05:47:30 +0200

    improved the code

--- script.js
+++ script.js
@@ -3,39 +3,54 @@ const guessInterface = document.getElementById('guessInterface');
 const declareInterface = document.getElementById('declareInterface');
 const declaration = document.getElementById('declaration');
 const playersGuess = document.getElementById('playersGuess');
-const playersNameSave = document.getElementById('playersNameSave');
+const gameStart = document.getElementById('gameStart');
 const playersName = document.getElementById('playersName');
-let player = "Player";
+let player;
 let random = Math.floor(Math.random()*100) + 1;
 let streak = 0;
+let gameFinished = false;
 function check (){
-    streak++;
+    streak++;//iterating the streak value each time the guess is being checked
     let playersInput = playersGuess.value;
+    if (isNaN(playersInput)||playersInput>100||playersInput<1) {
+        declaration.innerText = 'Please enter a valid number between 1 and 100.'
+    } else {
+        if (playersInput > random) {
+        declaration.innerText = 'No it is less';
+        } else if (playersInput < random) {
+        declaration.innerText = 'No it is more';
+        } else {
+        declaration.innerText = `Very nice ${player}! After ${streak} tries you had a correct guess!`;
+        document.getElementById('tryAgain').innerText = 'New game!';//renaming the button to new game after winning
+        random = Math.floor(Math.random()*100) + 1;//randomizing a new value for a new game
+        gameFinished = true;
+        }
+    }
     nameInterface.style.display = "none";
     guessInterface.style.display = "none";
     declareInterface.style.display = "block";
-    if (playersInput > random) {
-        declaration.innerText = 'No it is less';
-    } else if (playersInput < random) {
-        declaration.innerText = 'No it is more';
-    } else {
-        declaration.innerText = `Very nice ${player}! After ${streak} tries you had a correct guess!`;
-        document.getElementById('tryAgain').innerText = 'Play again!';
-        random = Math.floor(Math.random()*100) + 1;
-    }
 }
-playersNameSave.addEventListener('click', ()=>{
-    player = playersName.value;
-    nameInterface.style.display = "none";
-    declareInterface.style.display = "none";
-    guessInterface.style.display = "block";
-    document.getElementById('playersGuessLabel').innerText = `Alright ${player}, can you guess my secret number? Hint: it is between 1 and 100.`;
-});
-document.getElementById('tryAgain').addEventListener('click', ()=>{
+function again () {
+    if (gameFinished) { //starting a new game, resetting all values.
+        nameInterface.style.display = "block";
+        guessInterface.style.display = "none";
+        declareInterface.style.display = "none";
+        playersGuess.value ='';
+        player = "Player";
+        document.getElementById('tryAgain').innerText = 'Try again!';  
+    } else { //trying another guess
     nameInterface.style.display = "none";
     guessInterface.style.display = "block";
     declareInterface.style.display = "none";
     playersGuess.value =" ";
-    document.getElementById('tryAgain').innerText = 'Try again!';
+}
+}
+gameStart.addEventListener('click', ()=>{
+    player = playersName.value;
+    nameInterface.style.display = "none";
+    declareInterface.style.display = "none";
+    guessInterface.style.display = "block";
+    document.getElementById('playersGuessLabel').innerHTML = `Alright <span style = 'color:yellow'>${player}</span>, can you guess my secret number?<br>Hint: it is between 1 and 100.`;
 });
+document.getElementById('tryAgain').addEventListener('click', again);
 document.getElementById('check').addEventListener('click', check);
