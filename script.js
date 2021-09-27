6bdad5c39d51641ec2af6c6aaf6e0d93b24381d3
Mez Dek <mezdekosman@hotmail.com>
2021-09-27 04:46:44 +0200

    edited the script so it can work now and also edited the css to look better

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
+const nameInterface = document.getElementById('nameInterface');
+const guessInterface = document.getElementById('guessInterface');
+const declareInterface = document.getElementById('declareInterface');
+const declaration = document.getElementById('declaration');
+const playersGuess = document.getElementById('playersGuess');
+const playersNameSave = document.getElementById('playersNameSave');
+const playersName = document.getElementById('playersName');
 let player = "Player";
-const random = Math.floor(Math.random()*100) + 1;
-let gameFinished = false;
-
-function say (a) {
-    document.getElementById('nameInterface').style.display = "none";
-    document.getElementById('guessInterface').style.display = "none";
-    document.getElementById('declareInterface').style.display = "block";
-    if (a == 'less' || a == 'more') {
-    document.getElementById('declaration').innerText = `No it is ${a}`;
+let random = Math.floor(Math.random()*100) + 1;
+let streak = 0;
+function check (){
+    streak++;
+    let playersInput = playersGuess.value;
+    nameInterface.style.display = "none";
+    guessInterface.style.display = "none";
+    declareInterface.style.display = "block";
+    if (playersInput > random) {
+        declaration.innerText = 'No it is less';
+    } else if (playersInput < random) {
+        declaration.innerText = 'No it is more';
     } else {
-    document.getElementById('declaration').innerText = `${player} wins!`;
+        declaration.innerText = `Very nice ${player}! After ${streak} tries you had a correct guess!`;
+        document.getElementById('tryAgain').innerText = 'Play again!';
+        random = Math.floor(Math.random()*100) + 1;
     }
 }
-function submitGuess (){
-    let playerGuess = +document.getElementById('playerGuess').innerText;
-    let gameFinished = false;
-    while (!gameFinished){
-        if (playerGuess > random) {
-            say('less');
-        } else if (playerGuess < random) {
-            say('more');
-        } else {
-            say('win');
-            gameFinished = true;
-        }
-    }
-}
-document.getElementById('playerNameSave').addEventListener('click', ()=>{
-    player = document.getElementById('playerName').innerText;
-    document.getElementById('nameInterface').style.display = "none";
-    document.getElementById('declareInterface').style.display = "none";
-    document.getElementById('guessInterface').style.display = "block";
+playersNameSave.addEventListener('click', ()=>{
+    player = playersName.value;
+    nameInterface.style.display = "none";
+    declareInterface.style.display = "none";
+    guessInterface.style.display = "block";
+    document.getElementById('playersGuessLabel').innerText = `Alright ${player}, can you guess my secret number? Hint: it is between 1 and 100.`;
 });
-document.getElementById('submitGuess').addEventListener('click', submitGuess);
+document.getElementById('tryAgain').addEventListener('click', ()=>{
+    nameInterface.style.display = "none";
+    guessInterface.style.display = "block";
+    declareInterface.style.display = "none";
+    playersGuess.value =" ";
+    document.getElementById('tryAgain').innerText = 'Try again!';
+});
+document.getElementById('check').addEventListener('click', check);
